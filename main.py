b5b694ce70696f44cb1548be08ce2dd0085e2a11
fitriq751-max <fitriq751@gmail.com>
2026-03-13 11:35:53 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from book import Book
+import datetime
 
-buku1 = Book(1, "Belajar Python", "Affan", 2025)
+buku1 = Book(1, "Belajar Python", "Heri", 2025)
+buku2 = Book(2, "Belajar Java", "Andi", 2026)
 
 print(buku1.judul)
+print(buku1.get_update_terakhir())
+
+buku1.update_judul("python part 1")
+
+print(buku1.judul)
+print(buku1.get_update_terakhir())
